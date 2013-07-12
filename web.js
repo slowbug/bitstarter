0b92bb9d478006769e592b77980d58f25c69f268
slowbug <henrychen@webmage.com>
2013-07-12 04:33:54 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var inFile = "index.html"
+var inTxt = fs.readFileSync(inFile, 'utf-i');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(inTxt);
 });
 
 var port = process.env.PORT || 5000;
